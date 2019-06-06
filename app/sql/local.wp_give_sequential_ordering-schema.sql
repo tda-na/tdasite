1b3df2787f44d307fe943def2ac779d6f307c6fa
Getachew Gebreigziabher <getachew@iMac.local>
2019-06-06 18:01:46 -0400

    clean up

--- app/sql/local.wp_give_sequential_ordering-schema.sql
+++
@@ -1,8 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-
-CREATE TABLE `wp_give_sequential_ordering` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `payment_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
--- app/sql/local.wp_users.sql
+++
@@ -1,5 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_users` VALUES
-(1,"elsaadik","$P$BRSComw6FSwXmh29zyBS1ITVukwqNU/","elsaadik","elsaadik@yahoo.com","","2019-06-05 22:00:40","",0,"elsaadik");
